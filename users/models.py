006566a9ad722a813a260375ba8413160d76173e
lundincast <thecrazybaldhead15@hotmail.com>
2018-12-25 22:48:24 +0100

    Added uuid as id in custom user

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 class User(AbstractUser):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
